78d0ba56ea6533a2e32d0d8b3282a7f332e01083
Vaaiibhav Vaidya <vaaiibhav@techsparagus.com>
2022-01-27 16:02:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meditation Mitra' });
+  res.render('index', { title: 'Meditation Mitra Backend' });
 });
 
 module.exports = router;
